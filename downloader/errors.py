c25908bba030ed36c24f573b27bc8be0de100b3a
Farooq Mahmud - f0m0471 <Farooq.Mahmud@walmart.com>
2022-01-14 12:01:49 -0800

    Minor refactor.

--- downloader/errors.py
+++ downloader/errors.py
@@ -1,11 +1,3 @@
 class ChromeDriverNotFoundException(Exception):
-    """
-    Chromedriver.exe was not found in the expected path.
-    """
-
     def __init__(self, expected_path: str):
-        """
-        Class initializer.
-        :param expected_path: The expected path.
-        """
         super().__init__(f"Chromedriver.exe was not found at '{expected_path}'")
