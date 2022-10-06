1e64cfd30dbafea94a01501afd72616a38aac74c
Farooq <farooq@teqniqly.com>
2022-10-05 22:22:44 -0700

    Linting & version bump.

--- samples/sample.py
+++ samples/sample.py
@@ -8,15 +8,18 @@ from tq_scroll_scrape.scroll_and_scrape import ScrollAndScrape
 
 
 def save_file(source: str):
+    """
+            Saves the downloaded content to a file.
+    """
     filename = os.path.join(os.getcwd(), "sample.html")
 
-    with open(filename, "w") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         file.write(source)
 
 
-url = "https://www.espn.com/college-football/scoreboard/_/group/80/"
+URL = "https://www.espn.com/college-football/scoreboard/_/group/80/"
 driver_path = os.path.join(os.getcwd(), "../", "chromedriver.exe")
 scroll_scraper = ScrollAndScrape(driver_path)
-scroll_scraper.download(url, save_file, sleep_after_scroll_seconds=2, scroll_by=1000)
+scroll_scraper.download(URL, save_file, sleep_after_scroll_seconds=2, scroll_by=1000)
 scroll_scraper.driver.close()
 scroll_scraper.driver.quit()
